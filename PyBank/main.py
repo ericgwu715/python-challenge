17bef7b97944b830ab2d4eed1d729fa176bf21d3
Eric Wu <ericwu1995715@gmail.com>
2020-04-04 16:06:08 -0700

    homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,6 @@
 import os 
 import csv 
-
+import statistics
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
 with open(csvpath) as csvfile:
@@ -10,6 +10,12 @@ with open(csvpath) as csvfile:
     
     total = 0
     count = 0
+    top_increase = 0 
+    top_decrease = 0 
+    previous_row = 0
+    top_increase_month = ""
+    top_decrease_month = ""
+    list = []
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
@@ -17,7 +23,33 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         total = total + int(row[1])
         count = count + 1 
-    print(total)
-    print(count)
+    
+        current_increase =  int(row[1])-previous_row   
+        if current_increase > top_increase:
+            top_increase = current_increase
+            top_increase_month = row[0]
+        list.append(current_increase)
+        
+        if current_increase < top_decrease:
+            top_decrease = current_increase
+            top_decrease_month = row[0]
+        
+        
+        
+        
+        previous_row = int(row[1])
 
-
+    #print(total)
+    #print(statistics.mean(list))
+    #print(top_increase)
+    #print(top_increase_month)
+    #print(top_decrease_month)
+    #print(top_decrease)
+print("\n")
+print("Financial Analysis")
+print("-------------------------------")
+print("Total Month: " + str(count))
+print("Total: $" + str(total))
+print("Average Change: $" + str(statistics.mean(list)) )
+print("Greatest Increase in Profits: "+ top_increase_month + " ($" + str(top_increase) + ")")
+print("Greatest Decrease in Profits: "+ top_decrease_month + " ($" + str(top_decrease) + ")")
